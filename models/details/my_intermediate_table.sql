ffeb15b4ee6c601417905b1cfbba325f49ec35dd
yurui <rui.yu@notta.ai>
2026-02-05 17:03:03 +0800

    Update my_intermediate_table.sql

--- models/details/my_intermediate_table.sql
+++ models/details/my_intermediate_table.sql
@@ -1,4 +1,3 @@
-
 -- 中间表：用户统计
 {{ config(materialized='table') }}
 WITH
@@ -22,10 +21,9 @@ SELECT
 FROM `notta-data-analytics.dbt_models_details.user_details`
 WHERE pt=date_add(date(current_timestamp()),interval -1 DAY)
 AND is_create_ws=1 -- 选择创建ws的用户
-AND signup_date>='2025-11-20' -- 更改：测试的注册用户时间
--- AND signup_date<='2025-11-12' -- 更改：测试的注册用户时间
+AND signup_date>='2024-11-20' -- 【修正】将年份改为 2024 以确有数据
+-- AND signup_date<='2025-11-12' 
 AND first_transcription_type='Real Time'-- 首个转写是文件转写
--- 更改：测试改为下面多个语言了
 AND first_transcription_language IN ('en-US')
 ),
 
@@ -43,22 +41,16 @@ record AS (
        ,signup_platform
        ,device_category
        ,source
-       ,first_transcription_language -- 增加语言维度
-       -- 更改：不匹配测试条件的记录
+       ,first_transcription_language 
        ,count(CASE WHEN transcribe_language NOT IN ('en-US')
-                --    OR transcription_type!=2
                    OR engine='medical'
                    OR audio_duration>16200 THEN 1 ELSE NULL END) AS match_other_records
-       -- 更改：测试组 (11labs) 00-24 或 75-99
        ,count(CASE WHEN transcribe_language IN ('en-US')
-                --    AND transcription_type=2
                    AND engine!='medical'
                    AND audio_duration<=16200
                    AND (right(cast(workspace_id AS string),2) BETWEEN '50' AND '99')
                    THEN 1 ELSE NULL END) AS `test_group_11labs_records`
-       -- 更改：对照组 (ami)
        ,count(CASE WHEN transcribe_language IN ('en-US')
-                --    AND transcription_type=2
                    AND engine!='medical'
                    AND audio_duration<=16200
                    AND right(cast(workspace_id AS string),2) BETWEEN '00' AND '49'
@@ -79,7 +71,7 @@ record AS (
        ,signup_platform
        ,device_category
        ,source
-       ,first_transcription_language -- 增加语言维度
+       ,first_transcription_language 
 )
 
 SELECT
@@ -87,8 +79,7 @@ SELECT
  ,signup_platform
  ,device_category
  ,source
-  ,first_transcription_language -- 增加语言维度
-  -- 更改：更新分组名称
+ ,first_transcription_language 
  ,CASE WHEN match_other_records>0 THEN 'mixed records'
      WHEN test_group_11labs_records > 0 THEN 'assemblyai  only (Test)'
      WHEN control_group_ami_records > 0 THEN 'old only (Control)'
@@ -97,19 +88,18 @@ SELECT
  ,count(1) AS user_count
  ,count(distinct uid) AS user_check
  ,count(distinct CASE WHEN is_create_record=1 THEN uid ELSE NULL END) AS create_record_users
- ,count(distinct CASE WHEN is_trial=1 THEN uid ELSE NULL END) AS trial_users
+ ,count(distinct CASE WHEN is_trial=1 THEN uid ELSE NULL END) AS trial_users -- [TEST v4] 试用用户：统计在注册后 24 小时内开启 Trial 权限的去重用户数
  ,count(distinct CASE WHEN is_paid=1 AND datetime_diff(first_paid_plan_start_time,signup_time,HOUR)<=72 THEN uid ELSE NULL END) AS paid_users_in72hours
  ,count(distinct CASE WHEN is_paid=1 AND datetime_diff(first_paid_plan_start_time,signup_time,HOUR)<=96 THEN uid ELSE NULL END) AS paid_users_in96hours
  ,count(distinct CASE WHEN is_paid=1 AND datetime_diff(first_paid_plan_start_time,signup_time,HOUR)<=24 THEN uid ELSE NULL END) AS paid_users_in24hours
 FROM record
-WHERE match_other_records=0 -- 筛选出纯粹的测试用户
+WHERE match_other_records=0 
 GROUP BY
  signup_date
  ,signup_platform
  ,device_category
  ,source
- ,first_transcription_language -- 增加语言维度
-  -- 更改：更新分组名称
+ ,first_transcription_language 
  ,CASE WHEN match_other_records>0 THEN 'mixed records'
      WHEN test_group_11labs_records > 0 THEN 'assemblyai  only (Test)'
      WHEN control_group_ami_records > 0 THEN 'old only (Control)'
